b23b511ae108f57e57377cc390b3c960bead2fd0
kartik <kartik@darkfractal.com>
2024-06-26 12:52:03 +0530

    complete comment functionality and comment like functionality

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -4,6 +4,7 @@ import { doc, getDoc, getFirestore } from 'firebase/firestore';
 import { HiArrowLeft } from 'react-icons/hi';
 import Link from 'next/link';
 import Post from '@/components/Post';
+import Comments from '@/components/Comments';
 
 
 export default async function PostPage({ params }) {
@@ -21,7 +22,7 @@ export default async function PostPage({ params }) {
         <h2 className='sm:text-lg'>Back</h2>
       </div>
       <Post post={data} id={data.id} />
-      
+      <Comments id={params.id} />
     </div>
   );
 }
